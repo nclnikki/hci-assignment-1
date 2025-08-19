179f9b06b21a51a4087e24c8208d20484d35710e
Nicole Busa <nicolebusa13@gmail.com>
2025-08-19 13:55:24 +0800

    Make login/signup heading larger

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -13,8 +13,12 @@ function Dashboard() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello</h1>
-      <p>You are logged in!</p>
+      <h1 style={{ color: "white", fontSize: "50px", marginBottom: "10px" }}>
+        Hello!
+      </h1>
+      <p style={{ color: "white", fontSize: "18px" }}>
+        You are logged in.
+      </p>
       <button className="logout-btn" onClick={handleLogout}>Logout</button> 
 
     </div>
